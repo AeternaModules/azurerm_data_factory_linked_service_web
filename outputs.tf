3343fbfe52b2fd983f92b167abecb72a7e5ae6a2
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:35 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "data_factory_linked_service_webs_id" {
+  description = "Map of id values across all data_factory_linked_service_webs, keyed the same as var.data_factory_linked_service_webs"
+  value       = { for k, v in azurerm_data_factory_linked_service_web.data_factory_linked_service_webs : k => v.id }
+}
 output "data_factory_linked_service_webs_additional_properties" {
   description = "Map of additional_properties values across all data_factory_linked_service_webs, keyed the same as var.data_factory_linked_service_webs"
   value       = { for k, v in azurerm_data_factory_linked_service_web.data_factory_linked_service_webs : k => v.additional_properties }
